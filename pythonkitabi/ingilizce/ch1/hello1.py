3e15009d9e9789105e6670a3051e244e5caf6ee9
cartman <cartman@978fe5fe-063b-4661-968b-234df113938b>
2006-01-21 00:03:08 +0000

    Revert

--- pythonkitabi/ingilizce/ch1/hello1.py
+++ pythonkitabi/ingilizce/ch1/hello1.py
@@ -2,7 +2,7 @@ import sys
 from qt import *
 
 app=QApplication(sys.argv)
-button=QPushButton("Merhaba Dunya",None)
+button=QPushButton("Hello World",None)
 app.setMainWidget(button)
 button.show()
 app.exec_loop()
