b0c4e88163c8d89016de2709b0fc68fb85b561ee
Serdar Soytetir <obsoleteman@users.noreply.github.com>
2006-01-25 23:43:48 +0000

    örnek .py dosyaları da gelsin bakalım.

---
+++ pythonkitabi/türkçe/ch1/hello5.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+import sys
+from qt import *
+
+class HelloButton(QPushButton):
+
+    def __init__(self, *args):
+        apply(QPushButton.__init__, (self,) + args)
+        self.setText(unicode("Merhaba Dünya"))
+
+class HelloWindow(QMainWindow):
+    
+    def __init__(self, *args):
+        apply(QMainWindow.__init__, (self,) + args)
+        self.button=HelloButton(self)
+        self.setCentralWidget(self.button)
+        self.connect(self.button, SIGNAL("clicked()"),
+                     self, SLOT("close()"))
+    
+def main(args):
+    app=QApplication(args)
+    winlist=[]
+    for i in range(10):
+        win=HelloWindow()
+        win.show()
+        winlist.append(win)
+    
+    app.connect(app, SIGNAL("lastWindowClosed()"),
+                app, SLOT("quit()"))
+    app.exec_loop()
+  
+if __name__=="__main__":
+    main(sys.argv)
