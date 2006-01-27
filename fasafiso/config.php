6d8add9ec25b80f7d176e7a8d6bc0debf5c7ee7f
ahmet.aygun <ahmet.aygun@978fe5fe-063b-4661-968b-234df113938b>
2006-01-27 19:17:16 +0000

    config olmadan çalışmaz :)

---
+++ fasafiso/config.php
@@ -0,0 +1,25 @@
+<?php
+$config['core']['blogname']		= "Fasafiso";
+$config['core']['blogdesc']		= "Turkish Blog";
+$config['core']['path']			= "/home/fasafiso/public_html/";
+$config['core']['webaddress']		= "http://localhost/~fasafiso/";
+$config['core']['postperpage']		= "5";
+$config['core']['postsinfeed']		= "10";
+$config['core']['theme']		= "myweb";
+
+$config['db']['hostname']		= "localhost";
+$config['db']['port']			= "";
+$config['db']['username']		= "fasafiso";
+$config['db']['password']		= "fasafiso";
+$config['db']['databasename']		= "fasafiso";
+$config['db']['tableprefix']		= ""; //fasafiso_
+$config['db']['connectiontype']		= "persistent";
+
+$config['smarty']['libdir']		= "3rdparty/smarty";
+$config['smarty']['tpldir']		= "themes";
+$config['smarty']['compiledir']		= "tmp";
+$config['smarty']['cachedir']		= "tmp";
+$config['smarty']['plugindir']		= "plugins";
+$config['smarty']['caching']		= "false";
+$config['smarty']['forcecompile']	= "1";
+?>
