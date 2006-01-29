fb835677b345b3c27d088e6256cfd5e388eea88b
Gokmen Goksel <gokmen@goksel.me>
2006-01-29 02:09:36 +0000

    first hot stuff in next-generation: categories now fully works

--- fasafiso/admin/ng/index.php
+++ fasafiso/admin/ng/index.php
@@ -9,6 +9,17 @@
     }
 
     elseif (isset($_GET["categories"])) {
+        if (isset($_GET["edit"]) AND isset($_GET["id"])) set_smarty_vars ("editid",$_GET["id"]);
+        if (isset($_GET["add"])  AND isset($_POST["category"])):
+            if (add_category ($_POST["category"],$_POST["nicename"],$_POST["description"])) header ("location: ?categories");
+            else set_smarty_vars("error",ERROR);
+        elseif (isset($_GET["delete"]) AND isset($_GET["id"])):
+            if (del_category ($_GET["id"])) header ("location: ?categories");
+            else set_smarty_vars("error",ERROR);
+        elseif (isset($_GET["update"]) AND isset($_POST["category"])):
+            if (update_category ($_POST["id"],$_POST["category"],$_POST["nicename"],$_POST["description"])) header ("location: ?categories");
+            else set_smarty_vars("error",ERROR);
+        endif;
         $smarty->display("categories.html");
     }
 
--- fasafiso/admin/ng/utils.php
+++ fasafiso/admin/ng/utils.php
@@ -4,6 +4,8 @@
 
     /* Utils for FasaFiso Administration*/
 
+    /* USERS - START */
+
     function get_user_details($user,$passo,$state=FALSE){
         global $config;
         $uname = $passo;
@@ -15,10 +17,33 @@
         return perform_sql($sql_word);
     }
 
+    /* USERS - END */
+
+    /* CATEGORIES - START */
+
     function get_categories(){
 	global $config;
-	$sql_word = "SELECT * FROM {$config['db']['tableprefix']}category";
+	$sql_word = "SELECT * FROM {$config['db']['tableprefix']}category ORDER BY id";
         return perform_sql($sql_word);
     }
 
+    function add_category($category,$nicename,$description){
+        global $config;
+	$sql_word = "INSERT INTO {$config['db']['tableprefix']}category VALUES ('','{$category}', '{$nicename}', '{$description}')";
+        return @mysql_query($sql_word);
+    }
+
+    function del_category($id){
+        global $config;
+	$sql_word = "DELETE FROM {$config['db']['tableprefix']}category WHERE id='{$id}'";
+        return @mysql_query($sql_word);
+    }
+
+    function update_category($id,$category,$nicename,$description){
+        global $config;
+	$sql_word = "UPDATE {$config['db']['tableprefix']}category SET name='{$category}', nicename='{$nicename}', description='{$description}' WHERE id='$id'";
+        return @mysql_query($sql_word);
+    }
+
+    /* CATEGORIES - END */
 ?>
