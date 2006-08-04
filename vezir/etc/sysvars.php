b75ee03386f29327c7d6d399443499c3fed44d4a
Gokmen Goksel <gokmen@goksel.me>
2006-08-04 08:27:51 +0000

    vezir..

---
+++ vezir/etc/sysvars.php
@@ -0,0 +1,9 @@
+<?php
+
+    #Don't edit this file.
+    #Includes system variables.
+
+    $SYS['ProcFile'] = "proc.php";
+    $SYS['TState'] = 5;
+
+?>
---
+++ vezir/panel/index.php
@@ -0,0 +1,43 @@
+<?php
+
+    #Start the session
+    session_start();
+
+    #Get Config and Set Lang vars
+    require_once ('../etc/config.php');
+    require_once ('../lang/'.$LF); 
+
+    #Cleanup old sessions
+    session_unregister($SessionKeyword);
+    $_SESSION['state']="";
+    $_SESSION['loginName']="";
+
+    #Check Login or not
+    if (array_key_exists('login',$_GET)) {
+
+        #if Login, get sysvars and connection functions
+        require_once ('../etc/sysvars.php');
+        require_once ('../lib/vezir.php');
+
+        #create an instance for connection to database
+        $Vezir = new Vezir($CF);
+
+        #Check user and set session if ok
+        if ($Temp=$Vezir->GetUserDetails('Users',$_POST['username'],$_POST['password'],false)) {
+            session_register($SessionKeyword);
+            $_SESSION['Status'] = $Temp[0]['Status'];
+            $_SESSION['UserName'] = $Temp[0]['UserName'];
+            $_SESSION['RealName'] = $Temp[0]['RealName'];
+            $_SESSION['UserPhoto'] = $Temp[0]['PhotoLink'];
+            header("location: {$SYS['ProcFile']}");
+        }
+
+        #if wrong info given set message to warn user
+        else
+            $Message = USERNAME_OR_PASSWORD_WRONG;
+    }
+
+    #Get template file
+    include_once('template/login/index.tmpl');
+
+?>
