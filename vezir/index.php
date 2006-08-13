6866659d6e01a5c58c6ee753f7e91d8e4e426f32
Gokmen Goksel <gokmen@goksel.me>
2006-08-13 17:45:48 +0000

    oyle boyle...

--- vezir/index.php
+++ vezir/index.php
@@ -6,10 +6,46 @@
             <link href="template/azmesai/stil.css" rel="stylesheet" type="text/css">
         </head>
         <body>
+            <!--
             <div id="logo">
                 <img src="template/azmesai/images/logo.png">
+                <div id="vurgu">
+                    <div id="text">Sahte Ampuller; Kapalıyken gözü alacak kadar ışık yayması beklenen sahte ampuller..</div>
+                </div>
             </div>
-            <div id="pinfo"> </div>
+            -->
+            
+            <div id="headpicture">
+                <img src="template/azmesai/images/header.jpg">
+                <img src="template/azmesai/images/paper.jpg">
+            </div>
+            
+            <div id="container">
+            
+            <div id="left">
+            <div class="entry">
+                <div class="headline">
+                    <span class="title_">azmesai.net yayında</span>
+               </div>
+               <div class="content">
+                    Sonunda bu da oldu :) .. <a href="hh">RedHat Fedora</a>'sı için bi fon kurmuş maksat Fedora'nın ezikliğini 
+                    yoketmeye çalışmak olsa da fon olayı ile RedHat bir konuya daha değindi; RedHat olağan 
+                    zirvesinin üçüncü gününde Mark Webbink tarafından yapılan açıklamada Fedora'nın tamamen 
+                    ücretsiz ve RedHat'ten ticari yönden bağımsız olduğu açıklandı.
+                </div>
+                <div class="meta_">
+                    <span class="author_">M.Gökmen GÖKSEL</span>
+                   <span class="tags_">linux, redhat, fedora</span>
+                    <span class="comments_">2 yorum</span>
+                </div>
+            </div>
+            </div>
+            
+            <div id="right">
+                gokmen
+            </div>
+            
+            </div>
         </body>
     </html>
 
