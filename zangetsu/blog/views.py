271b68f51b7627241d14ff1c17450ba6add7b885
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2006-10-02 20:54:25 +0000

    arama

--- zangetsu/blog/views.py
+++ zangetsu/blog/views.py
@@ -1,1 +1,13 @@
 # -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from zangetsu.blog.models import Entry
+
+def search(request):
+    try:
+        search_term = request.GET['s']
+		# FIXME: Use paginator
+        search_results = Entry.objects.filter(content__icontains=search_term)[:20]
+    except:
+        search_results = None
+    return render_to_response('blog/entry_search.html', {'search_results': search_results})
+
