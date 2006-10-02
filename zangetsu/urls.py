23b3831d94805c72e666aeed1b45ebc8849742b7
caglar <caglar@978fe5fe-063b-4661-968b-234df113938b>
2006-10-02 00:32:06 +0000

    ...

--- zangetsu/urls.py
+++ zangetsu/urls.py
@@ -6,7 +6,7 @@ root = '/'.join(WEB_URL.split("/")[3:])
 
 urlpatterns = patterns('',
     (r'^%s/admin/' % root, include('django.contrib.admin.urls')),
-    (r'^%s/blog/' % root, include('blackrock.blog.urls')),
+    (r'^%s/blog/' % root, include('zangetsu.blog.urls')),
     (r'^%s/$' % root, 'django.views.generic.simple.redirect_to', {'url': '/%s/blog' % root}),
     (r'^%s/static/(.*)$' % root, 'django.views.static.serve', {'document_root': '%s/zangetsu/static' % DOCUMENT_ROOT, 'show_indexes': True}),
 )
