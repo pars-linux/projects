341fb15f0c6bc78582c656b2070de4ed50f94719
caglar <caglar@978fe5fe-063b-4661-968b-234df113938b>
2006-10-06 21:20:53 +0000

    wiki'miz olsun, reStructuredText desteklesin, dökümanları blog'a yazmak zorunda kalmayalım

---
+++ zangetsu/wiki/views.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render_to_response
+from zangetsu.wiki.models import WikiPage
+
+def index(request):
+    pages = WikiPage.objects.all().order_by('title')
+    return render_to_response('wiki/home.html', locals())
+
+def page(request, title):
+    try:
+        page = WikiPage.objects.get(title__exact=title)
+        return render_to_response('wiki/page.html', locals())
+    except WikiPage.DoesNotExist:
+        return HttpResponseRedirect("/wiki/edit/%s/" % title)
+
+def edit(request, title):
+    if request.POST:
+        try:
+            page = WikiPage.objects.get(title__exact=title)
+        except WikiPage.DoesNotExist:
+            page = WikiPage(title=title)
+        page.content = request.POST['content']
+        page.title = request.POST['title']
+        page.save()
+        return HttpResponseRedirect("/wiki/" + page.title + "/")
+    else:
+        try:
+            page = WikiPage.objects.get(title__exact=title)
+        except WikiPage.DoesNotExist:
+            page = WikiPage(title=title)
+            page.body = "<!-- Enter content here -->"
+        return render_to_response('wiki/edit.html', locals())
