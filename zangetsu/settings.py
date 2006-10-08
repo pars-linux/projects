e43aeb70b583ed249ed4bb97a2b3e024c24683ee
caglar <caglar@978fe5fe-063b-4661-968b-234df113938b>
2006-10-08 11:42:17 +0000

    no need to comment out variables

--- zangetsu/settings.py
+++ zangetsu/settings.py
@@ -24,8 +24,8 @@ DATABASE_PASSWORD = ''         # Not used with sqlite3.
 DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
-#CACHE_BACKEND = 'file:///%s/cache' % DOCUMENT_ROOT
-#CACHE_MIDDLEWARE_SECONDS=600
+CACHE_BACKEND = 'file:///%s/cache' % DOCUMENT_ROOT
+CACHE_MIDDLEWARE_SECONDS=600
 
 # Local time zone for this installation. All choices can be found here:
 # http://www.postgresql.org/docs/current/static/datetime-keywords.html#DATETIME-TIMEZONE-SET-TABLE
