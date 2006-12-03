e230bf565b75372783ba99e37cac5267fab1aea2
gokmen.goksel <gokmen.goksel@978fe5fe-063b-4661-968b-234df113938b>
2006-12-03 15:30:43 +0000

    * ajax,mintax,persil.

--- sunum/index.php
+++ sunum/index.php
@@ -3,31 +3,44 @@
     /** Gökmen GÖKSEL, gokmen@pardus.org.tr **/
 
     include 'tool.php';
+    $dir = "./presentations/";
 
-    $xml = get_xml( "sunum.xml");
-    if ($_GET["page"]<>"") $page=$_GET["page"]; else $page=0;
+    if (isset($_GET['file'])) {
+        $xml = get_xml($_GET['file']);
 
-    build_smarty();
-    build_defaults($xml->header,$xml->author,$xml->email,$xml->firm);
+        if (isset($_GET['pagelist'])) {
+            print get_page_list($xml,$_GET['file']);
+            return;
+        }
 
-    if (count($xml->page)>$page+1)
-        $right=$page+1;
+        if ($_GET["page"]<>"") $page=$_GET["page"]; else $page=0;
+
+        build_smarty();
+        build_defaults($xml->header,$xml->author,$xml->email,$xml->firm);
+
+        if (count($xml->page)>$page+1)
+            $right=$page+1;
+        else
+            $right=-1;
+        if ($page>0)
+            $left=$page-1;
+        else
+            $left =-1;
+
+        ssv("File",$_GET['file']);
+        ssv("RightNode",$right);
+        ssv("LeftNode" ,$left );
+        ssv("NextNode" ,get_node($xml,$page+1,"header"));
+        ssv("PrevNode" ,get_node($xml,$page-1,"header"));
+        ssv("PageHeader",xml2html(get_node($xml,$page,"header")));
+        ssv("Content"  , xml2html(get_node($xml,$page,"content")));
+        ssv("PageCount", $page." / ".(count($xml->page)-1));
+        $smarty->display("page.html");
+
+        return;
+    }
+
     else
-        $right=-1;
-    if ($page>0)
-        $left=$page-1;
-    else
-        $left =-1;
-
-    ssv("RightNode",$right);
-    ssv("LeftNode" ,$left );
-    ssv("NextNode" ,get_node($xml,$page+1,"header"));
-    ssv("PrevNode" ,get_node($xml,$page-1,"header"));
-    ssv("PageHeader",xml2html(get_node($xml,$page,"header")));
-    ssv("Content"  , xml2html(get_node($xml,$page,"content")));
-    ssv("PageCount", $page." / ".(count($xml->page)-1));
-    $smarty->display("page.html");
-
-
+        show_file_list($dir);
 
 ?>
