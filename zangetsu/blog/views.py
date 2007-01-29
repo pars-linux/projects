f95ca9d30d7930f82bdbe514da705c4ce193ba21
caglar <caglar@978fe5fe-063b-4661-968b-234df113938b>
2007-01-29 01:34:20 +0000

    title'larda da ara

--- zangetsu/blog/views.py
+++ zangetsu/blog/views.py
@@ -9,8 +9,8 @@ from zangetsu.blog.models import Entry
 def search(request):
     try:
         search_term = request.GET["s"]
-		# FIXME: Use paginator
-        search_results = Entry.objects.filter(content__icontains=search_term)[:20]
+        # FIXME: Use paginator
+        search_results = Entry.objects.filter(content__icontains=search_term, title__icontains=search_term)[:20]
     except:
         search_results = None
     return render_to_response("blog/entry_search.html", {"search_results": search_results})
