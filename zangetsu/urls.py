d824fe06c0eda2ed3ddcc76c1859ee94f04f4fbf
caglar <caglar@978fe5fe-063b-4661-968b-234df113938b>
2007-02-03 01:56:13 +0000

    revert last

--- zangetsu/urls.py
+++ zangetsu/urls.py
@@ -9,8 +9,8 @@ from zangetsu.settings import WEB_URL, DOCUMENT_ROOT
 root = "/".join(WEB_URL.split("/")[3:])
 
 urlpatterns = patterns("",
-    (r"^admin/", include("django.contrib.admin.urls")),
-    (r"^blog/", include("zangetsu.blog.urls")),
-    (r"^$", "django.views.generic.simple.redirect_to", {"url": "/%s/blog" % root}),
-    (r"^static/(.*)$", "django.views.static.serve", {"document_root": "%s/static" % DOCUMENT_ROOT, "show_indexes": True}),
+    (r"^%s/admin/" % root, include("django.contrib.admin.urls")),
+    (r"^%s/blog/" % root, include("zangetsu.blog.urls")),
+    (r"^%s/$" % root, "django.views.generic.simple.redirect_to", {"url": "/%s/blog" % root}),
+    (r"^%s/static/(.*)$" % root, "django.views.static.serve", {"document_root": "%s/static" % DOCUMENT_ROOT, "show_indexes": True}),
 )
