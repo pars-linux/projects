fb80f0fa6420be6359270bcb3bffe001e9c6efde
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2007-02-04 21:44:53 +0000

    pismillah

---
+++ zangetsu/wordpress2zangetsu.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright © 2006 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+import settings
+from blog.models import Entry, Tag
+import feedparser
+import datetime
+
+feed = feedparser.parse("rss.xml")
+
+# reverse the range so newest has biggest id
+length = range(len(feed["entries"]))
+length.reverse()
+
+for item in length:
+    e = feed["entries"][item]
+
+    entry = Entry()
+    entry.title = e.title
+
+    for i in e.categories:
+        t += Tag.objects.filter(title=i[i])
+
+    entry.content = e.summary
+    entry.pubdate = datetime.datetime(e.updated_parsed[0], e.updated_parsed[1], e.updated_parsed[2], e.updated_parsed[3], e.updated_parsed[4])
+    entry.save()
+
+    #needs to have a primary key value before a many-to-many relationship can be used.
+    entry.tag = t
+    entry.save()
