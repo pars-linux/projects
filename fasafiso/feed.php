3877c1ff66784505140e334a69537ae6fec2efa5
ahmet <ahmet@978fe5fe-063b-4661-968b-234df113938b>
2007-02-24 11:19:08 +0000

    Bunca zaman sonra fasafiso kurdurdunuz ya, helal olsun size, Gökmen ve Oğuz. Gökmen sana da iki çift lafım var, gördüğüm yerde bilahare ileteceğim.

--- fasafiso/feed.php
+++ fasafiso/feed.php
@@ -18,28 +18,28 @@ Header("Content-type: text/xml; charset=utf-8");
 <!-- generator="FasaFiso Feed Engine/FasaFiso v<?=$config['fasafiso']['version']?>" -->
 <rss version="2.0" xmlns:content="http://purl.org/rss/1.0/modules/content/" xmlns:wfw="http://wellformedweb.org/CommentAPI/" xmlns:dc="http://purl.org/dc/elements/1.1/">
 <channel>
-	<title><?=$config['core']['blogname']?></title>
-	<link><?=$config['core']['webaddress']?></link>
-	<description><?=$config['core']['blogdesc']?></description>
-	<pubDate><?php echo date("r"); ?></pubDate>
-	<generator>http://fasafiso.org/?version=<?=$config['fasafiso']['version']?></generator>	
-	<?php for($i = 0; $i < count($posts); $i++){ ?>
-	<item>
-		<title><?=$posts[$i]['title']?></title>
-		<link><?=$config['core']['webaddress']?>?id=<?=$posts[$i]['id']?></link>
-		<comments><?=$config['core']['webaddress']?>?id=<?=$posts[$i]['id']?>#comments</comments>
-		<pubDate><?=$posts[$i]['date']?></pubDate>
-		<dc:creator><?=$posts[$i]['author']?></dc:creator>
-		<category><?=$posts[$i]['category'][0]['name']?></category>
-		<guid isPermaLink="true"><?=$config['core']['webaddress']?>?id=<?=$posts[$i]['id']?></guid>
-		<description><?=htmlspecialchars(strip_tags($posts[$i]['entry']), ENT_QUOTES)?></description>
-		<content:encoded><?=htmlspecialchars($posts[$i]['entry'], ENT_QUOTES)?></content:encoded>
-<? /*<wfw:commentRSS>commentsrss</wfw:commentRSS>*/ ?>
-	</item>
-	<?php } ?>
+    <title><?=$config['core']['blogname']?></title>
+    <link><?=$config['core']['webaddress']?></link>
+    <description><?=$config['core']['blogdesc']?></description>
+    <pubDate><?php echo date("r"); ?></pubDate>
+    <generator>http://fasafiso.org/?version=<?=$config['fasafiso']['version']?></generator>    
+    <?php for($i = 0; $i < count($posts); $i++){ ?>
+    <item>
+        <title><?=$posts[$i]['title']?></title>
+        <link><?=$config['core']['webaddress']?>?id=<?=$posts[$i]['id']?></link>
+        <comments><?=$config['core']['webaddress']?>?id=<?=$posts[$i]['id']?>#comments</comments>
+        <pubDate><?=$posts[$i]['date']?></pubDate>
+        <dc:creator><?=$posts[$i]['author']?></dc:creator>
+        <?php for($j = 0; $j < count($posts[$i]['category']); $j++){ echo '<category>'.$posts[$i]['category'][$j]['name'].'</category>'; } ?>
+
+        <guid isPermaLink="true"><?=$config['core']['webaddress']?>?id=<?=$posts[$i]['id']?></guid>
+        <description><?=htmlspecialchars(strip_tags($posts[$i]['entry']), ENT_QUOTES)?></description>
+        <content:encoded><?=htmlspecialchars($posts[$i]['entry'], ENT_QUOTES)?></content:encoded>
+        <? /*<wfw:commentRSS>commentsrss</wfw:commentRSS>*/ ?>
+    </item>
+    <?php } ?>
 </channel>
 </rss>
 <?
-
 ob_end_flush();
 ?>
