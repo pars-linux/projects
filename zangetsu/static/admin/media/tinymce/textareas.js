54af184e4d7a05236e598a321cbee0e2e6c911b1
caglar <caglar@978fe5fe-063b-4661-968b-234df113938b>
2007-04-11 14:14:10 +0000

    entry alanı büyüsün

--- zangetsu/static/admin/media/tinymce/textareas.js
+++ zangetsu/static/admin/media/tinymce/textareas.js
@@ -2,5 +2,7 @@ tinyMCE.init({
     mode : "textareas",
     theme : "advanced",
     relative_urls : false,
-    convert_urls : false
+    convert_urls : false,
+    width : "800",
+    height : "500",
 });
