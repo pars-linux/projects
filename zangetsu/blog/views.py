f9a29e404d2f8e89497ea640f675dfdc0fcf0511
meren <meren@978fe5fe-063b-4661-968b-234df113938b>
2007-04-15 17:57:34 +0000

    - paginate search results..
      e.g. http://cekirdek.pardus.org.tr/~meren/blog/search/?s=pardus

--- zangetsu/blog/views.py
+++ zangetsu/blog/views.py
@@ -6,15 +6,35 @@
 
 from django.shortcuts import render_to_response
 from zangetsu.blog.models import Entry
+from django.core.paginator import ObjectPaginator, InvalidPage
+
+
+def build_paginator_dict(results, page = 0, item_per_page = 10):
+    paginator_result = ObjectPaginator(results, item_per_page)
+    return {"results": paginator_result.get_page(page),
+            "paginator": True,
+            "current_page": page,
+            "total_page": paginator_result.pages,
+            "total_results": paginator_result.hits,
+            "next": paginator_result.has_next_page(page),
+            "prev": paginator_result.has_previous_page(page)}
 
 def search(request):
     try:
         search_term = request.GET["s"]
-        # FIXME: Use paginator
         search_results = Entry.objects.filter(content__icontains=search_term) | Entry.objects.filter(title__icontains=search_term)
     except:
         search_results = None
-    return render_to_response("blog/entry_search.html", {"search_results": search_results[:20]})
+
+    try:
+        page = int(request.GET["p"])
+    except:
+        page = 0
+
+    paginator_dict = build_paginator_dict(search_results, page, 10)
+    response_dict = {"search_term": request.GET["s"]}
+    response_dict.update(paginator_dict)
+    return render_to_response("search/entry_search.html", response_dict)
 
 def recent_comments(request):
     return render_to_response("blog/recent_comments.html")
