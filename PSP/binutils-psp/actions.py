943d8864cf6e80aeb90713446936a844ac40a7f9
S.Çağlar Onur <caglar10ur@users.noreply.github.com>
2007-05-21 00:50:31 +0000

    MIPS arkadaşa i686 verince komik oluyor tabi...

--- PSP/binutils-psp/actions.py
+++ PSP/binutils-psp/actions.py
@@ -14,13 +14,21 @@ from pisi.actionsapi import get
 
 WorkDir = "binutils-2.16.1"
 
+def unset():
+    shelltools.export("CFLAGS", "")
+    shelltools.export("CXXFLAGS", "")
+
 def setup():
+    unset()
+
     shelltools.makedirs("%s/build-psp" % get.workDIR())
     shelltools.cd("%s/build-psp/" % get.workDIR())
 
     shelltools.system("%s/%s/configure --prefix=/opt/psp --target=psp --enable-install-libbfd" % (get.workDIR(), WorkDir))
 
 def build():
+    unset()
+
     shelltools.cd("%s/build-psp/" % get.workDIR())
 
     autotools.make()
--- PSP/newlib-psp/actions.py
+++ PSP/newlib-psp/actions.py
@@ -14,13 +14,20 @@ from pisi.actionsapi import get
 
 WorkDir = "newlib-1.13.0"
 
-def setup():
+def unset():
     shelltools.export("CFLAGS", "")
+    shelltools.export("CXXFLAGS", "")
+
+def setup():
+    unset()
+
     shelltools.makedirs("%s/build-psp" % get.workDIR())
     shelltools.cd("%s/build-psp/" % get.workDIR())
     shelltools.system("%s/%s/configure --prefix=/opt/psp --target=psp" % (get.workDIR(), WorkDir))
 
 def build():
+    unset()
+
     shelltools.cd("%s/build-psp/" % get.workDIR())
     autotools.make()
     
