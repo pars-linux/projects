99c00059279aa17692369379a5ffc38ef094240d
meren <meren@978fe5fe-063b-4661-968b-234df113938b>
2007-09-13 02:12:02 +0000

    very important fix :p svn up!

--- zangetsu/static/admin/media/tinymce/textareas-comments.js
+++ zangetsu/static/admin/media/tinymce/textareas-comments.js
@@ -1,6 +1,7 @@
 tinyMCE.init({
     mode : "textareas",
     theme : "advanced",
+    textarea_trigger : "tinyMCE_this",
     theme_advanced_buttons1 : "copy,paste,undo,redo,charmap,search,replace,|,link,unlink,image,|,code",
     theme_advanced_buttons2 : "formatselect,fontsizeselect,bold,italic,underline,strikethrough,sub,sup,|,justifyleft,justifycenter,justifyright,justifyfull,outdent,indent,|,bullist,numlist,|,forecolor,backcolor,removeformat",
     theme_advanced_buttons3 : "",
@@ -16,4 +17,7 @@ tinyMCE.init({
     extended_valid_elements : "a[name|href|target|title],img[class|src|border=0|alt|title|hspace|vspace|width|height|align|name],hr[class|width|size|noshade],font[face|size|color|style],span[class|align|style]",
     advimage_update_dimensions_onchange: true,
     plugins : "advimage,advlink,searchreplace,autosave,fullscreen",
+    handle_event_callback : "myHandleEvent",
+    onchange_callback : "myCustomOnChangeHandler",
+    oninit : "myCustomOnInit"
 });
