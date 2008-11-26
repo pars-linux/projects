9af1ca0c18d7515f7135a954987b89f438dbb389
serbulent <serbulent@978fe5fe-063b-4661-968b-234df113938b>
2008-11-26 16:00:13 +0000

    some additonal test situations for service script

--- test-manager/examples/bind/testScript1.py
+++ test-manager/examples/bind/testScript1.py
@@ -4,7 +4,7 @@
 import comar
 
 # This script is an example for testing applications 
-# which can be uses with 'service' command.
+# which can be uses with 'service' command. We test all situations of service script of bind server.
 
 class Test():
     def __init__(self):
@@ -13,9 +13,10 @@ class Test():
     def test1(self):
         result = {step:1, isSuccessful:False, comment:""}
         link = comar.Link()
-        command = 'service bind start'
+        command = 'service bind on'
         os.system(command)
-        if link.System.Service["bind"].info() == "started"
+        status == link.System.Service["bind"].info()
+        if status == "started" or status == "on"
             result['isSuccessful'] = True
         if not result['isSuccessful']:
             result['comment'] = command + " --- hatalı çalıştı"
@@ -23,6 +24,30 @@ class Test():
 
     def test2(self):
         result = {step:2, isSuccessful:False, comment:""}
+        link = comar.Link()
+        command = 'service bind off'
+        os.system(command)
+        status == link.System.Service["bind"].info()
+        if status == "stopped" or status == "off"
+            result['isSuccessful'] = True
+        if not result['isSuccessful']:
+            result['comment'] = command + " --- hatalı çalıştı"
+        self.resultList.append(result)
+
+    def test3(self):
+        result = {step:3, isSuccessful:False, comment:""}
+        link = comar.Link()
+        command = 'service bind start'
+        os.system(command)
+        status == link.System.Service["bind"].info()
+        if status == "started"
+            result['isSuccessful'] = True
+        if not result['isSuccessful']:
+            result['comment'] = command + " --- hatalı çalıştı"
+        self.resultList.append(result)
+
+    def test4(self):
+        result = {step:4, isSuccessful:False, comment:""}
         link = comar.Link()
         command = 'service bind stop'
         os.system(command)
@@ -32,8 +57,8 @@ class Test():
             result['comment'] = command + " --- hatalı çalıştı"
         self.resultList.append(result)
 
-    def test3(self):
-        result = {step:1, isSuccessful:False, comment:""}
+    def test5(self):
+        result = {step:5, isSuccessful:False, comment:""}
         link = comar.Link()
         command = 'service bind restart'
         os.system(command)
