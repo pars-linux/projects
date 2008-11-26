7238c1250f9bc9bd179875082a25a5f62efbc264
Serbulent Unsal <serbulent@users.noreply.github.com>
2008-11-26 07:56:56 +0000

    pismillah

---
+++ test-manager/testInitilizer.py
@@ -0,0 +1,78 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+
+from fetcher import Fetcher
+from confReader import ConfReader
+
+import os
+
+class InitList():
+    def __init__(self,packageList):
+        for package in packageList:
+            test = Initilizer(package)
+
+class Initilizer():
+    
+    def __init__(self,packageName):
+        #Define variables we need for the test
+        self.packageName = packageName
+        self.repo = "http://cekirdek.pardus.org.tr/~serbulent/test_guides/" + self.packageName + "/"
+        self.saveDir = "/tmp/testManager/" + self.packageName + "/"
+        self.filesDir =  self.saveDir + 'files/'
+        self.configFile =  self.saveDir + 'testProcess.conf'
+        #Create directory which we use in test if they are not exits
+        self.createDirs()
+        # First of all we have to fetch and read the config file for the package
+        self.fetcher = Fetcher()
+        url = self.repo +  'testProcess.conf'
+        self.fetcher.download(url, self.configFile )
+        cfr = ConfReader(self.configFile)
+        self.params = cfr.read()
+        self.fetchFiles()
+
+    def createDirs(self):
+        print "Creating directories..."
+        if not os.path.isdir("/tmp/testManager"):
+            try:
+                os.mkdir("/tmp/testManager")
+                print "/tmp/testManager created..."
+            except OSError:
+                errorString = "/tmp/testManager" + "  error on directory creation"
+                print errorString
+        if not os.path.isdir(self.saveDir):
+            try:
+                os.mkdir(self.saveDir)
+                print self.saveDir + " created..."
+            except OSError:
+                errorString = self.saveDir + "  error on directory creation"
+                print errorString
+        if not os.path.isdir(self.saveDir):
+            try:
+                os.mkdir(self.saveDir)
+                print self.saveDir + " created..."
+            except OSError:
+                errorString = self.saveDir + "  error on directory creation"
+                print errorString
+        if not os.path.isdir(self.filesDir):
+            try:
+                os.mkdir(self.filesDir)
+                print self.filesDir + " created.."
+            except OSError:
+                errorString = self.filesDir + "  error on directory creation"
+                print errorString
+
+    def fetchFiles(self):
+        if self.params['numberOfScripts']:
+            for i in range(1,self.params['numberOfScripts']+1):
+                scriptName = "testScript" + str(i) + ".py"
+                url = self.repo + scriptName
+                localPath = self.saveDir + scriptName
+                self.fetcher.download(url,localPath)
+
+        if self.params.has_key('files'):
+            for fileName in self.params['files']:
+                url = self.repo + "files/" + fileName
+                localPath = self.saveDir + scriptName
+                self.fetcher.download(url,localPath)
+
---
+++ test-manager/testManager.py
@@ -0,0 +1,87 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from __future__ import with_statement
+
+from PyQt4.QtCore import *
+from PyQt4.QtGui import *
+import ui_testManager
+
+import sys
+import pisi
+
+from packageTester import TestPackage
+from testInitilizer import InitList
+
+class testManager(QMainWindow,
+        ui_testManager.Ui_MainWindow):
+
+    def __init__(self, parent=None):
+        super(testManager, self).__init__(parent)
+        ui_testManager.Ui_MainWindow.__init__(self,parent)
+        self.setupUi(self)
+
+    def showTest(self,fileName):
+        url = QUrl("http://cekirdek.pardus.org.tr/~serbulent/test_guides/" + fileName + "/read.html")
+        self.trywebView.load(url)
+        self.trywebView.show()
+
+    @pyqtSignature("")
+    def on_action_Load_Test_triggered(self):
+        package_list = []
+        dialog = QFileDialog(self)
+        self.filename = dialog.getOpenFileName();
+        with open(self.filename) as f:
+            for line in f:
+                package_list.append(pisi.util.parse_package_name(line)[0])
+        # Download  test materials by InitList 
+        init = InitList(package_list)
+        self.pBrowser = PackageBrowser(package_list)
+        pCurrent = self.pBrowser.back()
+        self.showTest(pCurrent)
+        #TestPackage is the module where real test does
+        tp = TestPackage(pCurrent)
+
+    @pyqtSignature("")
+    def on_nextButton_clicked(self):
+        pCurrent = self.pBrowser.next()
+        self.showTest(pCurrent)
+        tp = TestPackage(pCurrent)
+
+    @pyqtSignature("")
+    def on_backButton_clicked(self):
+        pCurrent = self.pBrowser.back()
+        self.showTest(pCurrent)
+        tp = TestPackage(pCurrent)
+
+class PackageBrowser():
+
+    def __init__(self,package_list):
+        self.package_list = package_list
+        self.index = 0
+        self.min = 0
+        self.max = len(package_list) - 1
+
+    def next(self):
+        if self.index < self.max:
+            self.index += 1
+            return self.package_list[self.index]
+        if self.index == self.max:
+            return self.package_list[self.index]
+
+    def back(self):
+        if self.index == self.min:
+            return self.package_list[self.index]
+        if self.index > self.min:
+            self.index -= 1
+            return self.package_list[self.index]
+
+
+if __name__ == "__main__":
+
+    app = QApplication(sys.argv)
+    window = testManager()
+    window.show()
+    sys.exit(app.exec_())
+
+
