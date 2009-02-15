5d2dc09f4c6be566db4097c6bed0da354071b52f
Serbulent Unsal <serbulent@users.noreply.github.com>
2009-02-15 17:12:17 +0000

    bored

--- test-manager/simpleTestViewer/xmlMapper.py
+++ test-manager/simpleTestViewer/xmlMapper.py
@@ -16,6 +16,18 @@ class xmlMapper():
     def getTests(self):
         testDict = {}
         for tag in self.xmldoc.tags():
-            testDict[tag.getTagData("packageName")] = metaTest(tag.getTagData("packageName"), tag.getTagData("status"), tag.getTagData("comment"))
+            testDict[tag.getTagData("packageName")] = metaTest(tag.getTagData("packageName"), \
+                                                               tag.getTagData("status"), \
+                                                               tag.getTagData("comment"))
         return testDict
 
+    def setTests(self,dict):
+        for tag in self.xmldoc.tags():
+            # In our dict, we have metaTest class instances. We get attirbute names and values of this instances and create a xml tree
+            for key in dict.keys():
+                for k,v in dict[key].__dict__.items():
+                    node = tag.getTag(k)
+                    node.hide()
+                    tag.insertTag(k).insertData(v)
+
+        print self.xmldoc.toString()
