5b28b58031dde851d726bfbef704fa4a10c3b688
mesutcan.kurt <mesutcan.kurt@978fe5fe-063b-4661-968b-234df113938b>
2011-02-09 22:34:07 +0000

    fix adding repo problem to pisi

--- COMAK/packages/desktop/lxde/base/pardus-default-settings-lxde/comar/package.py
+++ COMAK/packages/desktop/lxde/base/pardus-default-settings-lxde/comar/package.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 from pisi import api as pisiapi
-from actionsapi import get
+import platform
+import subprocess
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    pisiapi.add_repo("lxde-repo", "http://x86-64.comu.edu.tr/lxde/%s/pisi-index.xml.xz" % get.ARCH())
+    #pisiapi.add_repo("lxde-repo", "http://x86-64.comu.edu.tr/lxde/%s/pisi-index.xml.xz" % platform.machine())
+    subprocess.Popen(["pisi ar lxde-repo http://x86-64.comu.edu.tr/lxde/%s/pisi-index.xml.xz" % platform.machine()], shell=True, stdout=subprocess.PIPE)
+
