f7ad1d00eec99eb71e5bafa3e0414e995cf39806
Özge Barbaros <ozgebarbaros@gmail.com>
2011-02-10 08:57:59 +0000

    sandbox hataları düzeltildi

--- COMAK/packages/desktop/gnome/base/gnome-menus/actions.py
+++ COMAK/packages/desktop/gnome/base/gnome-menus/actions.py
@@ -5,19 +5,16 @@
 # See the file http://www.gnu.org/copyleft/gpl.txt.
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-
-    autotools.autoreconf("-fi")
-    autotools.configure("--disable-static")
+    autotools.configure()
 
 def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "NEWS", "README")
