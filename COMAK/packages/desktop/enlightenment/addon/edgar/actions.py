e2b098a3535621c0ae0e3c47cc9b5e336e504e78
ozge.barbaros <ozge.barbaros@978fe5fe-063b-4661-968b-234df113938b>
2011-02-15 10:09:22 +0000

    actions.py icinde autotools.rawInstall(...) yerine autotools.install() yazildi

--- COMAK/packages/desktop/enlightenment/addon/edgar/actions.py
+++ COMAK/packages/desktop/enlightenment/addon/edgar/actions.py
@@ -19,5 +19,5 @@ def build():
     autotools.make()
 
 def install():
-    autotools.rawInstall("DESTDIR=%s"%get.installDIR)
-
+    #autotools.rawInstall("DESTDIR=%s"%get.installDIR)
+    autotools.install()
