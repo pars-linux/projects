388fc079c292afca08608347091092579da01e14
gokhan.akgun <gokhan.akgun@978fe5fe-063b-4661-968b-234df113938b>
2011-02-22 12:11:08 +0000

    scrMouse.py icin plugin olusturuldu.

--- COMAK/kaptan/src/kaptan/plugins/kde.py
+++ COMAK/kaptan/src/kaptan/plugins/kde.py
@@ -9,17 +9,43 @@
 #
 # Please read the COPYING file.
 #
-
+from PyQt4.QtCore import QString
 from PyKDE4.kdecore import KConfig
+from PyKDE4.kdeui import KGlobalSettings
 
 class KdePlugin:
 
     def __init__(self):
         self._keyboard_config = KConfig("kxkbrc")
-
+        self.mouse_config_hand = KConfig("kcminputrc")
+        self.mouse_config_singleclick= KConfig("kdeglobals")
+    
     def getKeyboardLayoutList(self):
         group = self._keyboard_config.group("Layout")
         return str(group.readEntry("LayoutList"))
+
+    def getMouseSingleClick(self):
+        group = self.mouse_config_singleclick.group("KDE")
+        return str(group.readEntry("SingleClick"))
+    
+    def getMouseHand(self):
+        group = self.mouse_config_hand.group("Mouse")
+        return group.readEntry("MouseButtonMapping")
+
+    def setMouseHand(self,mouseHand):
+        group = self.mouse_config_hand.group("Mouse")
+        group.writeEntry("MouseButtonMapping",QString(mouseHand))
+        self.mouse_config_hand.sync()
+
+    def setMouseSingleClick(self,clickBehaviour):
+        group= self.mouse_config_singleclick.group("KDE")
+        group.writeEntry("SingleClick",str(clickBehaviour))
+        self.mouse_config_singleclick.sync()
+    
+    def setReverseScrollPolarity(self,isChecked):
+        group = self.mouse_config_hand.group("Mouse")
+        group.writeEntry("ReverseScrollPolarity",QString(str(isChecked)))
+        self.mouse_config_hand.sync()
 
     def setKeyboardLayoutList(self, layoutList, lastLayout):
         if lastLayout:
@@ -40,6 +66,8 @@ class KdePlugin:
             group.writeEntry("LayoutList",layoutList)
             group.writeEntry("DisplayNames",layoutList)
             self._keyboard_config.sync()
-
         return True
+
+    def emitChange(self):
+        KGlobalSettings.self().emitChange(KGlobalSettings.SettingsChanged,KGlobalSettings.SETTINGS_MOUSE)
 
