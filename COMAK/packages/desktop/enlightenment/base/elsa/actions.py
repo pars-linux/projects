df37ca0d139289b8f60fba8bd4e7ad1f57454750
Necdet Yucel <necdetyucel@gmail.com>
2011-02-22 14:16:56 +0000

    cogunlukla kozmetik

--- COMAK/packages/desktop/enlightenment/base/elsa/actions.py
+++ COMAK/packages/desktop/enlightenment/base/elsa/actions.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
+# Copyright 2011 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh --sysconfdir=/etc --prefix=/usr")
+    shelltools.system("./autogen.sh \
+                       --sysconfdir=/etc \
+                       --prefix=/usr")
 
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-    pisitools.dodoc("AUTHORS", "ChangeLog", "README")
 
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README", "COPYING")
