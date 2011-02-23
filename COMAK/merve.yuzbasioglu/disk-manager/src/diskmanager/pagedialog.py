25f427324cef727f77ca8c0f9c9caa09ee0fe5e3
merve.yuzbasioglu <merve.yuzbasioglu@978fe5fe-063b-4661-968b-234df113938b>
2011-02-23 11:55:28 +0000

    yorum satirlari eklendi

--- COMAK/merve.yuzbasioglu/disk-manager/src/diskmanager/pagedialog.py
+++ COMAK/merve.yuzbasioglu/disk-manager/src/diskmanager/pagedialog.py
@@ -1,5 +1,3 @@
-#ifndef PAGEDIALOG.PY
-#define PAGEDIALOG.PY
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
@@ -16,36 +14,33 @@
 # PyQt
 from PyQt4 import QtCore
 from PyQt4 import QtGui
-from PyQt4.QtGui import QDialog
 
-
+#Pds Stuff
 from diskmanager.context import *
-
 import context as ctx
 
 # Edit widget
 from diskmanager.edit import EditWidget
 
 if ctx.Pds.session == ctx.pds.Kde4:
-
+    #PyKDE
     from PyKDE4 import kdeui
     from PyKDE4 import kdecore
 
     class PageDialog(kdeui.KPageDialog):
         def __init__(self, parent):
             kdeui.KPageDialog.__init__(self, parent)
+
             self.setFaceType(kdeui.KPageDialog.Tabbed)
-
             self.setCaption(i18n("Settings"))
-            self.page_widget = EditWidget(self)
 
+            self.page_widget = EditWidget(self)
             self.page_item = kdeui.KPageWidgetItem(self.page_widget, i18n("Settings"))
 
             self.addPage(self.page_item)
-
             self.edit = self.page_widget
+
 else:
-    _fromUtf8 = QtCore.QString.fromUtf8
     class PageDialog(QtGui.QDialog):
         def __init__(self,parent):
             QtGui.QDialog.__init__(self,parent)
@@ -55,15 +50,17 @@ else:
             self.tab=QtGui.QTabWidget(self)
             self.tab.addTab(self.page_widget,i18n("Settings"))
 
+            #Buttons
             self.buttonBox = QtGui.QDialogButtonBox(self)
             self.buttonBox.setGeometry(QtCore.QRect(4, 152, 540, 25))
             self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
             self.layout=QtGui.QVBoxLayout(self)
             self.layout.addWidget(self.tab)
             self.layout.addWidget(self.buttonBox)
-            self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
-            QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), self.accept)
-            QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), self.reject)
+            self.buttonBox.setObjectName(i18n("buttonBox"))
+
+            #SIGNAL
+            QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(i18n("accepted()")), self.accept)
+            QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(i18n("rejected()")), self.reject)
             QtCore.QMetaObject.connectSlotsByName(self)
             self.edit=self.page_widget
-#endif // PAGEDIALOG.PY
