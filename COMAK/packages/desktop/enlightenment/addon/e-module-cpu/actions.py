c7c102107b5e0c17144a713356d6fe30d7dc8551
ozge.barbaros <ozge.barbaros@978fe5fe-063b-4661-968b-234df113938b>
2011-02-23 12:38:06 +0000

    add tr patch

--- COMAK/packages/desktop/enlightenment/addon/e-module-cpu/actions.py
+++ COMAK/packages/desktop/enlightenment/addon/e-module-cpu/actions.py
@@ -10,7 +10,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir='cpu'
 
 def setup():
     shelltools.system("./autogen.sh \
