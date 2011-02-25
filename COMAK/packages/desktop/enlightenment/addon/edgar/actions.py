c4e529e9974777d6d0686bbe0d04a51d4c49e85b
Necdet Yucel <necdetyucel@gmail.com>
2011-02-25 07:48:17 +0000

    dodoc some stuff, but needs some more work

--- COMAK/packages/desktop/enlightenment/addon/edgar/actions.py
+++ COMAK/packages/desktop/enlightenment/addon/edgar/actions.py
@@ -1,24 +1,21 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
+# Copyright 2011 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
-from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
-    shelltools.system("./autogen.sh \
-                        --disable-static \
-                        --prefix=/usr")
-
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
 
 def install():
-    #autotools.rawInstall("DESTDIR=%s"%get.installDIR)
     autotools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "README")
