e52b1c4a046b1e66400b38858efaa4a1f719cf66
gokhan.akgun <gokhan.akgun@978fe5fe-063b-4661-968b-234df113938b>
2011-03-01 00:49:50 +0000

    enlightenment icin hazırlanmıs kaptanın hali hazırdaki kaynak dosyaları

---
+++ COMAK/kaptan-e17/src/kaptan/screens/scrGoodbye.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
+#Pds Stuff
+import kaptan.screens.context as ctx
+from kaptan.screens.context import *
+
+if ctx.Pds.session == ctx.pds.Kde4:
+    from PyKDE4.kdecore import ki18n, KGlobal, KConfig
+
+import subprocess, sys
+
+from kaptan.screen import Screen
+from kaptan.screens.ui_scrGoodbye import Ui_goodbyeWidget
+import kaptan.screens.scrSmolt as smoltWidget
+
+class Widget(QtGui.QWidget, Screen):
+    title = i18n("More")
+    desc = i18n("Congratulations!")
+
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self,None)
+        self.ui = Ui_goodbyeWidget()
+        self.ui.setupUi(self)
+
+       # lang = KGlobal.locale().language()
+
+        lang=QLocale().language()
+    
+        if lang == 125:
+            self.helpPageUrl = "http://www.pardus.org.tr/destek"
+        else:
+            self.helpPageUrl = "http://www.pardus.org.tr/eng/support"
+
+        self.smoltUrl = "http://smolt.pardus.org.tr:8090"
+
+    def on_buttonSystemSettings_clicked(self):
+        self.procSettings = QProcess()
+        self.procSettings.start("systemsettings")
+
+    def on_buttonHelpPages_clicked(self):
+	#self.procSettings = QProcess()
+        #command = "openURL (" + self.helpPageUrl+")"
+        #self.procSettings.start(command)
+	QtGui.QDesktopServices().openUrl(QUrl(self.helpPageUrl))
+    def on_buttonSystemSettings_2_clicked(self):
+        #self.procSettings = QProcess()
+        #command = "openURL (" + self.smoltUrl+")"
+        #self.procSettings.start(command)
+	QtGui.QDesktopServices().openUrl(QUrl(self.smoltUrl))
+    
+    def setSmolt(self):
+      # Smolt Settings
+        if self.smoltSettings["profileSend"]:
+            self.procSettings = QProcess()
+            command = "smoltSendProfile"
+            arguments = ["-a", "--submitOnly"]
+            self.procSettings.startDetached(command, arguments)
+
+#       if not self.smoltSettings["profileSend"]:
+#            self.ui.smoltGroupBox.hide()
+#            self.ui.label.hide()
+
+    def shown(self):
+       self.smoltSettings = smoltWidget.Widget.screenSettings
+       self.setSmolt()
+
+    def execute(self):
+       return True
+       
+
+
---
+++ COMAK/kaptan-e17/src/kaptan/screens/scrPackage.py
@@ -0,0 +1,131 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from PyQt4 import QtGui
+from PyQt4.QtGui import QMessageBox
+from PyQt4.QtCore import *
+        
+import kaptan.screens.context as ctx
+from kaptan.screens.context import *
+
+from kaptan.screen import Screen
+from kaptan.screens.ui_scrPackage import Ui_packageWidget
+
+import subprocess
+import pisi
+import comar
+import platform
+isUpdateOn = False
+
+class Widget(QtGui.QWidget, Screen):
+    title = i18n("Packages")
+    desc = i18n("Install / Remove Programs")
+
+    # min update time
+    updateTime = 12
+
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self,None)
+        self.ui = Ui_packageWidget()
+        self.ui.setupUi(self)
+        self.ui.checkBox.setChecked(False)
+        self.flagRepo = 0
+        self.repoName = "enlightenment-repo"
+        self.repoAddress ="http://x86-64.comu.edu.tr/enlightenment/%s/pisi-index.xml.xz" % platform.machine()
+        self.ui.information_label.setText("")
+        # create a db object
+        self.repodb = pisi.db.repodb.RepoDB()
+        n = 1 # temporary index variable for repo names
+        self.connect(self.ui.checkBox,SIGNAL("stateChanged(int)"),self.slotEnlightenmentRepo)
+
+        # control if we already have enlightenmentn repo
+        if self.repodb.has_repo(self.repoName):
+            #self.pushDelete.setEnabled(0)
+            self.ui.checkBox.setEnabled(0)
+            errorMessage= i18n("enlightenment-repo isimli Depo sisteminizde mevcuttur.")
+            self.ui.information_label.setText(errorMessage)
+    
+    def controlRepo(self):
+        print "kontrol ediliyor..."
+        if self.repodb.has_repo_url(self.repoAddress):
+            self.ui.checkBox.setCheckState(False)
+            errorMessage= i18n("enlightenment-repo isimli Depo sisteminizde mevcuttur.")
+            self.ui.information_label.setText(errorMessage)
+            return False
+        else:
+            # control if we already have the same repo name
+            if self.repodb.has_repo(self.repoName):
+                tmpRepoName = self.repoName
+                # if so, try to give a name like "enlightenmentn"
+                for r in self.repodb.list_repos():
+                    if self.repodb.has_repo(tmpRepoName):
+                        tmpRepoName = self.repoName + str(n)
+                        n = n +1
+                    else:
+                        break
+                self.repoName = tmpRepoName
+            return True
+
+    def slotEnlightenmentRepo(self):
+        if self.ui.checkBox.isChecked():
+            if not self.addRepo(self.repoName, self.repoAddress):
+                self.flagRepo = 1
+                self.ui.checkBox.setChecked(0)
+                errorTitle = i18n("Authentication Error")
+                errorMessage= i18n("You are not authorized for this operation.")
+                self.messageBox = QMessageBox(errorTitle, errorMessage, QMessageBox.Critical, QMessageBox.Ok, 0, 0)
+                self.messageBox.show()
+        else:
+            if self.flagRepo != 1:
+                self.removeRepo(self.repoName)
+
+    def addRepo(self, r_name, r_address):
+            try:
+                link = comar.Link()
+                link.setLocale()
+                link.System.Manager["pisi"].addRepository(r_name, r_address)
+                self.ui.information_label.setText("enlightenment-repo adli depo , depo listenize eklendi.")
+                return True
+            except:
+                return False
+
+    def removeRepo(self, r_name):
+        if self.controlRepo():
+            try:
+                link = comar.Link()
+                link.setLocale()
+                link.System.Manager["pisi"].removeRepository(r_name)
+                self.ui.information_label.setText("enlightenment-repo adli depo , depo listesinden silindi.")
+                return True
+            except:
+                return False
+
+
+#    def applySettings(self):
+#        # write selected configurations to future package-managerrc
+#        if ctx.Pds.session == ctx.pds.Kde4:
+#
+#            config = PMConfig()
+#            config.setSystemTray(QVariant(self.ui.showTray.isChecked()))
+#            config.setUpdateCheck(QVariant(self.ui.checkUpdate.isChecked()))
+#            config.setUpdateCheckInterval(QVariant(self.ui.updateInterval.value() * 60))
+#
+#            if self.ui.showTray.isChecked():
+#                p = subprocess.Popen(["package-manager"], stdout=subprocess.PIPE)
+
+    def shown(self):
+        pass
+
+    def execute(self):
+ #       self.applySettings()
+        return True
+
