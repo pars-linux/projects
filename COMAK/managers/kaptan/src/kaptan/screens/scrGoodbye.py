be8b0d1e04d4835d15aea1c07efe2514bbb30519
Orçun Avşar <orc.avs@gmail.com>
2011-03-03 17:32:11 +0000

    böyle bir commit yapılmadı

---
+++ COMAK/managers/kaptan/src/kaptan/screens/scrGoodbye.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+#
+
+from PyQt4 import QtGui
+from PyQt4.QtCore import *
+
+#Pds Stuff
+import kaptan.screens.context as ctx
+from kaptan.screens.context import *
+from kaptan.plugins import Desktop
+
+import subprocess, sys
+
+from kaptan.screen import Screen
+from kaptan.screens.ui_scrGoodbye import Ui_goodbyeWidget
+import kaptan.screens.scrSmolt as smoltWidget
+
+class Widget(QtGui.QWidget, Screen):
+    title = i18n("More")
+    desc = i18n("Congratulations!")
+
+    def __init__(self, *args):
+        QtGui.QWidget.__init__(self,None)
+        self.ui = Ui_goodbyeWidget()
+        self.ui.setupUi(self)
+
+        lang = Desktop.common.getLanguage()
+
+        if lang == 125:
+            self.helpPageUrl = "http://www.pardus.org.tr/destek"
+        else:
+            self.helpPageUrl = "http://www.pardus.org.tr/eng/support"
+
+        self.smoltUrl = "http://smolt.pardus.org.tr:8090"
+
+    def on_buttonSystemSettings_clicked(self):
+        self.procSettings = QProcess()
+        self.procSettings.start("systemsettings")
+
+    def on_buttonHelpPages_clicked(self):
+        Desktop.common.showUrl(self.helpPageUrl)
+    def on_buttonSystemSettings_2_clicked(self):
+        Desktop.common.showUrl(self.smoltUrl)
+        
+        #Qt
+        #QtGui.QDesktopServices().openUrl(QUrl(self.smoltUrl))
+    def setSmolt(self):
+        if not self.smoltSettings["profileSend"]:
+            self.ui.smoltGroupBox.hide()
+            self.ui.label.hide()
+
+    def shown(self):
+       self.smoltSettings = smoltWidget.Widget.screenSettings
+       self.setSmolt()
+
+    def execute(self):
+       return True
+
+
