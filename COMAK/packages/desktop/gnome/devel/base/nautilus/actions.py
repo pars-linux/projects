0d2b68a4e423257fa3e4cbddda6c6a377bc7a01f
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-03-05 21:29:22 +0000

    add Burak Caliskan's packages

---
+++ COMAK/packages/desktop/gnome/devel/base/nautilus/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static\
+                         --disable-packagekit\
+                         --disable-update-mimedb\
+                         --enable-introspection \
+                         --disable-more-warnings")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    #pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
