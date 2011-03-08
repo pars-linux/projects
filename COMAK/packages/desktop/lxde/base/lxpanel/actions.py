89b58e381a4984814ab39ef3fcb961ad796d9685
Mesutcan Kurt <mesutcank@gmail.com>
2011-03-08 06:19:29 +0000

    add missing lxde/base packages from pardus review

---
+++ COMAK/packages/desktop/lxde/base/lxpanel/actions.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-alsa \
+                        --with-x\
+                        --with-plugins=all \
+                        --enable-man")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+    pisitools.dodoc("AUTHORS", "ChangeLog", "README")
+
