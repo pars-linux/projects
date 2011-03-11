4ec6696bbc19edb9efe50b0780ec665156e2f670
engin.manap <engin.manap@978fe5fe-063b-4661-968b-234df113938b>
2011-03-11 12:35:31 +0000

    remove eweather from urgent packages. cosmetic changes on e-module-exalt-client

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-exalt-client/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-exalt-client/actions.py
@@ -5,7 +5,8 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
-from pisi.actionsapi import pisitools, shelltools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
