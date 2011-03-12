06395c1575a15f683c31c29aa63e6323c653c276
Mesutcan Kurt <mesutcank@gmail.com>
2011-03-12 09:31:36 +0000

    do not add lxde-repo with this package

--- COMAK/packages/desktop/lxde/devel/base/pardus-default-settings-lxde/comar/package.py
+++ COMAK/packages/desktop/lxde/devel/base/pardus-default-settings-lxde/comar/package.py
@@ -6,9 +6,6 @@ import platform
 import subprocess
 
 def postInstall(fromVersion, fromRelease, toVersion, toRelease):
-    #pisiapi.add_repo("lxde-repo", "http://x86-64.comu.edu.tr/lxde/%s/pisi-index.xml.xz" % platform.machine())
-    subprocess.Popen(["pisi ar lxde-repo http://x86-64.comu.edu.tr/lxde/%s/pisi-index.xml.xz" % platform.machine()], shell=True, stdout=subprocess.PIPE)
-    
     environments = open ("/usr/lib/python2.7/site-packages/pds/environments.py","r")
     environments_array = environments.readlines()
     x= 0
