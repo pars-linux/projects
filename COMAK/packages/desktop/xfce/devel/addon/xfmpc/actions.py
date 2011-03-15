f6a7ea5e2d95883c5bf82ffa20f96c771b087d44
Alper Tekinalp <alper.tekinalp@gmail.com>
2011-03-15 09:11:00 +0000

    used files in contrib 2009

--- COMAK/packages/desktop/xfce/devel/addon/xfmpc/actions.py
+++ COMAK/packages/desktop/xfce/devel/addon/xfmpc/actions.py
@@ -1,16 +1,15 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
+# Copyright 2011 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
-from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
 
 def build():
     autotools.make()
@@ -18,3 +17,4 @@ def build():
 def install():
     autotools.install()
 
+    pisitools.dodoc("COPYING", "AUTHORS", "README", "ChangeLog", "NEWS")
