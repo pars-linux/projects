515d5d983f6434234a91f02a63053f5709e2b64f
mehmet.samat <mehmet.samat@978fe5fe-063b-4661-968b-234df113938b>
2011-03-17 16:28:34 +0000

    add patch

---
+++ COMAK/packages/desktop/gnome/devel/addon/brasero/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/BILGEM
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.configure("--disable-scrollkeeper \
+            --disable-caches \
+            --disable-dependency-tracking \
+            --with-gtk=2.0 \
+            --disable-search \
+            --enable-cdrtools \
+            --enable-cdrkit")
+
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
