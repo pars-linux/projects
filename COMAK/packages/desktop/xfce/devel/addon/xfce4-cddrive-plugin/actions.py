251d1665ed53692321f576bcdc1030344c86d836
Mesutcan Kurt <mesutcank@gmail.com>
2011-03-17 01:05:54 +0000

    add xfce4-cddrive-plugin

---
+++ COMAK/packages/desktop/xfce/devel/addon/xfce4-cddrive-plugin/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    pisitools.dosed("configure", "exo-0.3", "exo-1")
+    autotools.configure()
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
+
