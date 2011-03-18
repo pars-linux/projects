d2d7568e822a593736dc3088e59ff054e802c63b
Mesutcan Kurt <mesutcank@gmail.com>
2011-03-18 16:25:34 +0000

    add thunar-wallpaper-plugin

---
+++ COMAK/packages/desktop/xfce/devel/addon/thunar-wallpaper-plugin/actions.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/copyleft/gpl.txt.
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import get
+from pisi.actionsapi import pisitools
+
+def setup():
+    pisitools.dosed("configure", "thunarx-1", "thunarx-2")
+
+    autotools.configure("-with-pic")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
+
