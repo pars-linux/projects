daf624bc190e1298f282fd572b9cd573b49d25a2
Kaan Özdinçer <kaanozdincer@gmail.com>
2011-03-22 15:33:50 +0000

    Fix files path and Add rawInstall

--- COMAK/packages/desktop/xfce/devel/addon/xfce4-mixer/actions.py
+++ COMAK/packages/desktop/xfce/devel/addon/xfce4-mixer/actions.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
 
 def setup():
     autotools.configure("--disable-dependency-tracking")
@@ -15,7 +16,7 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("README", "AUTHORS", "THANKS", "ChangeLog", "COPYING", "TODO", "NEWS", "HACKING")
 
