c9ab2844303b7886b9c23d4b8122d07d2227277f
kaan.ozdincer <kaan.ozdincer@978fe5fe-063b-4661-968b-234df113938b>
2011-03-22 14:21:03 +0000

    Add xfce4-mixer package

---
+++ COMAK/packages/desktop/xfce/devel/addon/xfce4-mixer/actions.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2010 TUBITAK/BILGEM
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    autotools.configure("--disable-dependency-tracking")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("README", "AUTHORS", "THANKS", "ChangeLog", "COPYING", "TODO", "NEWS", "HACKING")
+
