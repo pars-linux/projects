bf654f5b87974960ccaf9dd53322245d0b3ee318
sertac.turkel <sertac.turkel@978fe5fe-063b-4661-968b-234df113938b>
2011-03-23 19:46:09 +0000

    fluxbox.py eklendi

---
+++ COMAK/managers/kaptan/src/kaptan/plugins/fluxbox.py
@@ -0,0 +1,199 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2005-2009, TUBITAK/UEKAE
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation; either version 2 of the License, or (at your option)
+# any later version.
+#
+# Please read the COPYING file.
+
+import os,sys
+import time
+import re
+import piksemel
+from PyQt4.QtCore import *
+from . import base
+
+from kaptan.tools.desktop_parser import DesktopParser
+from kaptan.screens.scrStyle import Widget as scrStyleWidget
+
+#CONFIG_KEYBOARD = QSettings("lxsession/LXDE","desktop")
+#CONFIG_MOUSE_lefthanded = QSettings("lxsession/LXDE","desktop")
+#CONFIG_WALLPAPER = QSettings("pcmanfm/LXDE","pcmanfm")
+
+HEAD_SCREENS = ['scrWelcome', 'scrMouse', 'scrStyle', 'scrWallpaper']
+TAIL_SCREENS = ['scrSummary', 'scrGoodbye']
+
+#libfm configuration files
+CONFIG_LIBFM = QSettings("%s/.config/libfm/libfm.conf"%os.environ["HOME"], QSettings.IniFormat)
+
+
+
+def save_openboxrc(tree):
+    pass
+def save_lxsession(new):
+    pass
+def update_lxsession():
+    pass
+# end of shared LXDE methods
+
+class Keyboard(base.Keyboard):
+    pass
+
+class Mouse(base.Mouse):
+
+    def getMouseHand(self):
+        '''get mouse hand from conf file'''
+        return CONFIG_LIBFM.value("Mouse/LeftHanded")
+
+    def setMouseHand(self,mouseHand):
+        '''set mouse hand to conf file'''
+        CONFIG_LIBFM.setValue("Mouse/LeftHanded",QString(mouseHand))
+        CONFIG_LIBFM.sync()
+
+    def setMouseSingleClick(self,clickBehaviour):
+        '''set single/double click choice'''
+        new_value = (not clickBehaviour and 1) or 0
+        CONFIG_LIBFM.setValue("config/single_click",new_value)
+        CONFIG_LIBFM.sync()
+        return True
+
+    def getMouseSingleClick(self):
+        '''get if single/double click is choiced'''
+        single_click = CONFIG_LIBFM.value("config/single_click").toInt()[0]
+        return single_click and True or False
+
+    def setReverseScrollPolarity(self,isChecked):
+        '''set reverse scroll polarity'''
+        CONFIG_LIBFM.setValue("Mouse/ReverseScrollPolarity",QString(str(isChecked)))
+        CONFIG_LIBFM.sync()
+
+class Wallpaper(base.Wallpaper):
+
+    def getWallpaperSettings(self):
+
+        dir = QDir("/usr/share/fluxbox/backgrounds")
+        dir.setFilter( QDir.NoSymLinks | QDir.Files )
+        a = QStringList()
+        a.append ("*.jpeg")
+        a.append("*.jpg")
+        a.append("*.png")
+        dir.setNameFilters(a)
+        lst = dir.entryList()
+        items = []
+        for desktopFile in lst:
+            wallpaper = {}
+
+            try:
+                wallpaper["wallpaperTitle"] = "picture"
+            except:
+                wallpaper["wallpaperTitle"] = "picture1"
+
+            try:
+                wallpaper["wallpaperDesc"] = "description"
+            except:
+                wallpaper["wallpaperDesc"] = "description1"
+
+                # Get all files in the wallpaper's directory
+            thumbFolder = desktopFile
+
+            #    """
+            #    Appearantly the thumbnail names doesn't have a standart.
+            #    So we get the file list from the contents folder and
+            #    choose the file which has a name that starts with "scre".
+
+            #    File names I've seen so far;
+            #    screenshot.jpg, screnshot.jpg, screenshot.png, screnshot.png
+            #    """
+            wallpaper["wallpaperThumb"] = "/usr/share/fluxbox/backgrounds/%s"%desktopFile
+            wallpaper["wallpaperFile"] = desktopFile
+            items.append(wallpaper)
+        return items
+
+
+    def setWallpaper(self ,wallpaper):
+        if wallpaper:
+            os.popen("fbsetbg %s" %wallpaper)
+        #if color :
+        #    os.popen("pcmanfm --wallpaper-mode %s" % color)
+            print wallpaper
+class Common(base.Common):
+
+    def getLanguage(self):
+        locale_app = QLocale()
+        locale_os = QLocale.system()
+        info = []
+        var = QLocale.languageToString(locale_app.language())
+        return var
+class Style(base.Style):
+
+    def getDesktopNumber(self):
+        return 3
+
+    def setDesktopNumber(self):
+        pass
+    def setThemeSettings(self):
+        pass
+    def setStyleSettings(self):
+        pass
+    def setDesktopType(self):
+        pass
+
+    def reconfigure(self):
+       # commands = ["pcmanfm --desktop-off",
+       #             "pcmanfm -d --desktop",
+       #             "lxsession -r",
+       #             "openbox --restart"]
+       # command = ";".join(commands)
+       # os.system(command)
+        pass
+class Package(base.Package):
+
+    def example(self):
+        pass
+
+class Menu(base.Menu):
+
+    def getMenuStyle(self):
+        pass
+
+    def setMenuSettings(self):
+        pass
+
+def test_config_files():
+    print ">libfm options..."
+    print " reading mouse single_click"
+    single_click = CONFIG_LIBFM.value("config/single_click").toInt()[0]
+    print " single_click=",single_click
+    print " reversing option..."
+    new_value = (not single_click and 1) or 0
+    CONFIG_LIBFM.setValue("config/single_click",new_value)
+    print " new value=",CONFIG_LIBFM.value("config/single_click").toString()
+    CONFIG_LIBFM.sync()
+    print " sync worked..."
+
+    print ">openbox options..."
+    print " reading theme/name option"
+    #theme_name = CONFIG_OPENBOX.getTag("theme").getTag("name").firstChild().data()
+    print " theme/name=%s"%theme_name
+    print " setting value to 'onyx'"
+   # CONFIG_OPENBOX.getTag("theme").getTag("name").setData("onyx")
+    #save_openboxrc(CONFIG_OPENBOX)
+    print " file saved..."
+
+    print " reading desktops/number"
+    #desktop_number = CONFIG_OPENBOX.getTag("desktops").getTag("number").firstChild().data()
+    print " desktop number=%s"%desktop_number
+    print " setting desktop number to 3"
+    #CONFIG_OPENBOX.getTag("desktops").getTag("number").setData("3")
+    #save_openboxrc(CONFIG_OPENBOX)
+    print " file saved..."
+
+
+
+if __name__ == "__main__":
+    if "--test" in sys.argv:
+        test_config_files()
+
