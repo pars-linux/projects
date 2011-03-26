0b215bfd5f5e73241374cc36bcdea36154d84b88
merve.yuzbasioglu <merve.yuzbasioglu@978fe5fe-063b-4661-968b-234df113938b>
2011-03-26 12:54:55 +0000

    update disk-manager package

--- COMAK/packages/desktop/comak/devel/disk-manager-comak/actions.py
+++ COMAK/packages/desktop/comak/devel/disk-manager-comak/actions.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2006-2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import pythonmodules
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
 
-WorkDir="disk-manager"
+shelltools.export("HOME", get.workDIR())
+
 def install():
     pythonmodules.install()
