a93aa819337529192cf3bdba7c146169433dd9f6
ozge.barbaros <ozge.barbaros@978fe5fe-063b-4661-968b-234df113938b>
2011-03-27 13:16:31 +0000

    cosmetic changes

--- COMAK/packages/desktop/enlightenment/devel/addon/edgar/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/edgar/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2011 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -11,11 +10,9 @@ from pisi.actionsapi import autotools
 def setup():
     autotools.autoreconf("-fiv")
     autotools.configure("--disable-static")
-
 def build():
     autotools.make()
 
 def install():
     autotools.install()
-
     pisitools.dodoc("AUTHORS", "COPYING", "README")
