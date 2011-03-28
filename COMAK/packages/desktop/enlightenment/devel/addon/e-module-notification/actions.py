f5080416690f8150ed6a28bf278bf2443930b698
engin.manap <engin.manap@978fe5fe-063b-4661-968b-234df113938b>
2011-03-28 03:06:53 +0000

    Version bump.

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-notification/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-notification/actions.py
@@ -9,8 +9,10 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
+WorkDir="notification"
+
 def setup():
-    shelltools.export("AUTOPOINT", "/bin/true")
+    #shelltools.export("AUTOPOINT", "/bin/true")
 
     autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
