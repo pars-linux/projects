38ac6c0dcc744d924219baae7e7a677026cfacef
engin.manap <engin.manap@978fe5fe-063b-4661-968b-234df113938b>
2011-03-28 03:17:35 +0000

    Version bump.

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-diskio/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-diskio/actions.py
@@ -9,6 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+WorkDir="diskio" 
+
 def setup():
         #shelltools.export("AUTOPOINT", "/bin/true")
         autotools.autoreconf("-vfi")
