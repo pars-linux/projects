d29dbac33578ce5afe8b0aea416d03b87ea6b0c2
engin manap <enginmanap@gmail.com>
2011-03-28 03:06:25 +0000

    Version bump.

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-deskshow/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-deskshow/actions.py
@@ -9,6 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+WorkDir="deskshow" 
+
 def setup():
         shelltools.export("AUTOPOINT", "/bin/true")
         autotools.autoreconf("-vfi")
