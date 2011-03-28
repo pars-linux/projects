0d07189002fc08ce8484ca713a4ea1d49727c171
merve.yuzbasioglu <merve.yuzbasioglu@978fe5fe-063b-4661-968b-234df113938b>
2011-03-28 15:28:46 +0000

    fix e-module-trash

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-trash/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-trash/actions.py
@@ -8,8 +8,6 @@ from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
-WorkDir = "e-module-trash"
-
 def setup():
     autotools.autoreconf("-fi")
     autotools.configure("--disable-static")
