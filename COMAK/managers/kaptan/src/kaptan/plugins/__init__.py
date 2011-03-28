aeca3cd8ee7e9d62d1665e84a544e051da6fdb99
sertac.turkel <sertac.turkel@978fe5fe-063b-4661-968b-234df113938b>
2011-03-28 20:45:39 +0000

    init e gnome eklendi context.py düzenlendi

--- COMAK/managers/kaptan/src/kaptan/plugins/__init__.py
+++ COMAK/managers/kaptan/src/kaptan/plugins/__init__.py
@@ -20,6 +20,7 @@ __plugins = {
             pds.LXDE : "lxde",
             pds.Enlightenment : "enlightenment",
             pds.Fluxbox: "fluxbox",
+            pds.Gnome: "gnome",
             #pds.DefaultDe:"kde", #FIXME: temporary
             }
 
