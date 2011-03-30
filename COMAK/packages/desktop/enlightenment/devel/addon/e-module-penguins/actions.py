645c703868219001fcefad7a3ce26f87642b01e1
merve.yuzbasioglu <merve.yuzbasioglu@978fe5fe-063b-4661-968b-234df113938b>
2011-03-30 12:01:35 +0000

    fix penguins module

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-penguins/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-penguins/actions.py
@@ -1,18 +1,15 @@
 #/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
-from pisi.actionsapi import shelltools
-WorkDir="penguins" 
 
+WorkDir="penguins"
 def setup():
-    shelltools.export("AUTOPOINT", "/bin/true")
     autotools.autoreconf("-vfi")
     autotools.configure("--disable-static")
 
