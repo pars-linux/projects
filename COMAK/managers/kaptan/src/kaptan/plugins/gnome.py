cfb0bf3d5cf58e717fa5fcebc9b5e07eed01f702
sertac.turkel <sertac.turkel@978fe5fe-063b-4661-968b-234df113938b>
2011-03-30 15:24:36 +0000

    resim onizlemeleri getirildi,eksik resimler var

--- COMAK/managers/kaptan/src/kaptan/plugins/gnome.py
+++ COMAK/managers/kaptan/src/kaptan/plugins/gnome.py
@@ -92,9 +92,9 @@ class Wallpaper(base.Wallpaper):
             if string.find(desktopFiles, "background-default")<0:
                 if string.find(desktopFiles, "jpg") > -1 :
                     desktopFiles=desktopFiles.replace("jpg","png")
-                    wallpaper["wallpaperThumb"] ="/usr/share/kde4/apps/kaptan/kaptan/gnome_previews/nature/%s"%desktopFiles.split("/")[-1]
+                    wallpaper["wallpaperThumb"] ="/usr/share/kde4/apps/kaptan/kaptan/gnome_previews/%s"%desktopFiles.split("/")[-1]
                 else :
-                    wallpaper["wallpaperThumb"] ="/usr/share/kde4/apps/kaptan/kaptan/gnome_previews/abstract/%s"%desktopFiles.split("/")[-1]
+                    wallpaper["wallpaperThumb"] ="/usr/share/kde4/apps/kaptan/kaptan/gnome_previews/%s"%desktopFiles.split("/")[-1]
                 wallpaper["wallpaperFile"] = desktopFiles
                 items.append(wallpaper)
         return items
