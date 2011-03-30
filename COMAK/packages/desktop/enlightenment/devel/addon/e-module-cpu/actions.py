2946d46dab97db633bbdcfce822aa5606a613539
ozge.barbaros <ozge.barbaros@978fe5fe-063b-4661-968b-234df113938b>
2011-03-30 12:04:07 +0000

    del line 'shelltools.export(AUTOPOINT, /bin/true)'

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-cpu/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-cpu/actions.py
@@ -13,7 +13,6 @@ from pisi.actionsapi import shelltools
 WorkDir="cpu" 
 
 def setup():
-    shelltools.export("AUTOPOINT", "/bin/true")
     autotools.autoreconf("-fiv")
     autotools.configure("--disable-static")
 
