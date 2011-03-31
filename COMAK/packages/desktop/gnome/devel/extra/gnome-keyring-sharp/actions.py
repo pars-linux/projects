161b63994fc7e7ff2df98dabf83f878425608a05
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-03-31 09:00:47 +0000

    add docs

--- COMAK/packages/desktop/gnome/devel/extra/gnome-keyring-sharp/actions.py
+++ COMAK/packages/desktop/gnome/devel/extra/gnome-keyring-sharp/actions.py
@@ -5,6 +5,7 @@
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
 
 def setup():
     autotools.configure("--disable-static")
@@ -15,3 +16,4 @@ def build():
 def install():
     autotools.install()
 
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
