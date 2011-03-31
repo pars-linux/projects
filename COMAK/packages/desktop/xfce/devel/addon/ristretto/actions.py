8471e00bdedba98a74a03ca38cca4c5b0a7b23e1
Necdet Yucel <necdetyucel@gmail.com>
2011-03-31 08:08:51 +0000

    fix files paths, dodocs some stuff, cosmetics

--- COMAK/packages/desktop/xfce/devel/addon/ristretto/actions.py
+++ COMAK/packages/desktop/xfce/devel/addon/ristretto/actions.py
@@ -1,14 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
-from pisi.actionsapi import get
 
 def setup():
     shelltools.system("./autogen.sh")
@@ -18,4 +16,6 @@ def build():
 
 def install():
     autotools.install()
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "NEWS", "README", "TODO")
 
