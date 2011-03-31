9eab6835fb4a16624492f124a91398d983b9fe4c
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-03-31 07:43:24 +0000

    cosmetic

--- COMAK/packages/desktop/enlightenment/devel/addon/e-module-deskshow/actions.py
+++ COMAK/packages/desktop/enlightenment/devel/addon/e-module-deskshow/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2011 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -9,7 +8,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import autotools
 from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
-WorkDir="deskshow" 
+
+WorkDir="deskshow"
 
 def setup():
         shelltools.export("AUTOPOINT", "/bin/true")
