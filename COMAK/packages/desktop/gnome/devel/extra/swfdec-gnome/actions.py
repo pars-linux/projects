d684041af9f772be3fb863eda0f06b131afaa677
mehmet.samat <mehmet.samat@978fe5fe-063b-4661-968b-234df113938b>
2011-03-31 11:54:40 +0000

    configure parametresi eklendi

--- COMAK/packages/desktop/gnome/devel/extra/swfdec-gnome/actions.py
+++ COMAK/packages/desktop/gnome/devel/extra/swfdec-gnome/actions.py
@@ -10,7 +10,7 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure()
+    autotools.configure("--disable-static")
     pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
 def build():
