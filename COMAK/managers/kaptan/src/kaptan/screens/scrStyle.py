4f529bff74271b2f1315345d2d7c48306c59fbf5
sertac.turkel <sertac.turkel@978fe5fe-063b-4661-968b-234df113938b>
2011-04-01 22:19:19 +0000

    kaptan.py dosyasina tasinan kod parcasi silindi.

--- COMAK/managers/kaptan/src/kaptan/screens/scrStyle.py
+++ COMAK/managers/kaptan/src/kaptan/screens/scrStyle.py
@@ -58,17 +58,6 @@ class Widget(QtGui.QWidget, Screen):
             self.ui.listIcon.item(0).setHidden(1)
         elif ctx.Pds.session.Name == "KDE":
             self.ui.listIcon.item(1).setHidden(1)
-        if ctx.Pds.session.Name == "gnome":
-            kd= os.getenv("HOME")
-            #kaptanrc
-            QSettings.setPath(QSettings.IniFormat, QSettings.UserScope,kd+"/.kaptanrc" )
-            kaptanConfig = QSettings(kd+"/.kaptanrc" ,QSettings.IniFormat)
-            start = kaptanConfig.value("General/RunOnStart").toString()
-            if start != "False":
-                os.popen("gconftool-2 --type=string --set /apps/metacity/general/theme Orta" )
-                os.popen("gconftool-2 --type string --set /desktop/gnome/interface/gtk_theme Orta")
-                os.popen("gconftool-2 --type string --set /desktop/gnome/interface/icon_theme Faenza-Dark")
-
         self.ui.label.setText(str(ctx.Pds.session.Name)+" Themes")
         self.styleDetails = {}
         self.catLang = Desktop.common.getLanguage()
