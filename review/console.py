3d949b89a83213b8f28cfd3b73fd04b35fad369f
bahadir.kandemir <bahadir.kandemir@978fe5fe-063b-4661-968b-234df113938b>
2011-04-08 11:34:28 +0000

    Example console application

---
+++ review/console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""
+    Example console application
+"""
+
+# Standard modules
+import sys
+
+# Exit codes
+SUCCESS, FAILURE = range(0, 2)
+
+# Main
+def main():
+    """
+        Main loop
+    """
+    return SUCCESS
+
+if __name__ == '__main__':
+    sys.exit(main())
