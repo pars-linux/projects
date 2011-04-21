f89982e2fbab5bdf1c9030357cc85226a3d9b390
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-04-21 21:15:12 +0000

    add empathy

---
+++ COMAK/packages/desktop/gnome3/devel/addon/empathy/actions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2
+# See the file http://www.gnu.org/copyleft/gpl.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+
+def setup():
+    autotools.autoreconf("-fiv")
+    autotools.configure("--disable-static \
+                        --disable-control-center-embedding \
+                        --disable-Werror \
+                        --enable-silent-rules \
+                        --disable-coding-style-checks \
+                        --disable-schemas-compile \
+                        --disable-call \
+                        --disable-location \
+                         --enable-spell=yes")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "CONTRIBUTORS", "COPYING", "README", "NEWS", "TODO")
