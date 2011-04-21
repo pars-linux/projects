59f10112651790273213d4a6db524e1d08150a9c
Necdet Yucel <necdetyucel@gmail.com>
2011-04-21 12:48:31 +0000

    Enable introspection

--- COMAK/packages/desktop/gnome3/devel/base/libgnomekbd/actions.py
+++ COMAK/packages/desktop/gnome3/devel/base/libgnomekbd/actions.py
@@ -9,7 +9,8 @@ from pisi.actionsapi import pisitools
 from pisi.actionsapi import get
 
 def setup():
-    autotools.configure("--disable-static")
+    autotools.configure("--disable-static \
+                         --enable-introspection=yes")
 
     pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
 
