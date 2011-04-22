3c2123517ebb1df8d3ab69964dc03fd452b0da2d
Necdet Yucel <necdetyucel@gmail.com>
2011-04-22 14:30:37 +0000

    add gnome-bluetooth

---
+++ COMAK/packages/desktop/gnome3/devel/base/gnome-bluetooth/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+
+def setup():
+    #autotools.autoreconf("-fiv")
+    autotools.configure("--enable-introspection=yes \
+                        --enable-nautilus \
+                        --disable-moblin \
+                        --disable-desktop-update \
+                        --disable-icon-update \
+                        --disable-schemas-compile")
+
+    pisitools.dosed("libtool", " -shared ", " -Wl,--as-needed -shared ")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README")
+
