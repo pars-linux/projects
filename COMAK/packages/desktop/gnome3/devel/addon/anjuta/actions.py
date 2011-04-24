52a31aba30109d440fad695cee7eab1f3a4a36be
Necdet Yucel <necdetyucel@gmail.com>
2011-04-24 23:10:39 +0000

    add anjuta from contrib/2009, version bump, split headers, package take over

---
+++ COMAK/packages/desktop/gnome3/devel/addon/anjuta/actions.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+
+def setup():
+    autotools.autoreconf("-fiv")
+    shelltools.system("intltoolize --force --copy --automake")
+    autotools.configure("--disable-schemas-install \
+                         --disable-scrollkeeper \
+                         --enable-gtk-doc")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.install()
+
+    pisitools.remove("/usr/share/icons/hicolor/icon-theme.cache")
+
+    pisitools.dodoc("ABOUT-NLS", "AUTHORS", "ChangeLog", "COPYING", "FUTURE", "MAINTAINERS", "NEWS", "README", "ROADMAP", "THANKS", "TODO")
