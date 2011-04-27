e1f1fa2366c65ce1dd955e7c00725193525229c8
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-04-27 17:31:35 +0000

    version bump

--- COMAK/packages/desktop/gnome3/base/gsettings-desktop-schemas/actions.py
+++ COMAK/packages/desktop/gnome3/base/gsettings-desktop-schemas/actions.py
@@ -17,5 +17,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("README","NEWS","MAINTAINERS")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README", "NEWS", "MAINTAINERS")
 
