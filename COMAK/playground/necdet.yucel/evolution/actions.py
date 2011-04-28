591279d61cbc0aba71db8836fd5aa99d81972b4f
Necdet Yucel <necdetyucel@gmail.com>
2011-04-28 14:26:30 +0000

    alperden calalim evolution'u

---
+++ COMAK/playground/necdet.yucel/evolution/actions.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+#shelltools.export("CFLAGS", "%s -ledataserver" % get.CFLAGS())
+
+def setup():
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-scrollkeeper \
+                        --enable-nss=yes \
+                        --with-openldap=yes \
+                        --enable-smime=yes \
+                        --with-krb5=/usr")
+
+def build():
+    autotools.make("-I/usr/include/evolution-data-server-2.32")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "MAINTAINERS", "NEWS", "README", "TODO")
