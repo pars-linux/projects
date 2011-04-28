59dc5458df4f55b654b8a9f443f5f69a11c5d92d
mehmet.samat <mehmet.samat@978fe5fe-063b-4661-968b-234df113938b>
2011-04-28 10:10:53 +0000

    add brasero

---
+++ COMAK/playground/mehmet.samat/brasero/actions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
+from pisi.actionsapi import shelltools
+
+shelltools.export("HOME", get.workDIR())
+
+def setup():
+    autotools.configure("--prefix=/usr \
+                         --sysconfdir=/etc \
+                         --localstatedir=/var \
+                         --disable-caches \
+                         --disable-schemas-compile \
+                         --disable-introspection")
+def build():
+    autotools.make("-j1")
+
+def install():
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
+
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING*", "README")
