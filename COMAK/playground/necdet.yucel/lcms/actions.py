e89159871217838446d5f3b8f8c63b613b4ac502
Necdet Yucel <necdetyucel@gmail.com>
2011-04-29 12:17:10 +0000

    add lcms from 2011, version bump

---
+++ COMAK/playground/necdet.yucel/lcms/actions.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+#
+# Copyright 2005-2010 TUBITAK/UEKAE
+# Licensed under the GNU General Public License, version 2.
+# See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
+
+from pisi.actionsapi import autotools
+from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
+from pisi.actionsapi import get
+
+def setup():
+    #needed by mozilla patch
+    autotools.autoreconf("-vfi")
+    autotools.configure("--disable-dependency-tracking \
+                         --disable-static \
+                         --with-jpeg \
+                         --with-tiff \
+                         --with-zlib")
+
+def build():
+    autotools.make()
+
+def install():
+    autotools.rawInstall('DESTDIR=%s \
+                          BINDIR=%s/usr/bin \
+                          includedir=/usr/include' % (get.installDIR(), get.installDIR()))
+
+    pisitools.insinto("/usr/share/lcms/profiles", "testbed/*.icm")
+
+    pisitools.dodoc("AUTHORS", "README*", "NEWS", "doc/*")
