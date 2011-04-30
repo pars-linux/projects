61cdd9851c4e57daa85afdaacbca56a4ea04c016
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-04-30 18:17:12 +0000

    version bump, add translations.xml

--- COMAK/packages/desktop/gnome3/base/gucharmap/actions.py
+++ COMAK/packages/desktop/gnome3/base/gucharmap/actions.py
@@ -6,7 +6,10 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.autoreconf("-fiv")
@@ -17,7 +20,7 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("ChangeLog", "NEWS", "README", "TODO")
 
