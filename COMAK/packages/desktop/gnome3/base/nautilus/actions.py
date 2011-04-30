ea9f61ce65b0a92c1c2c9797c044df153eef1102
Necdet Yucel <necdetyucel@gmail.com>
2011-04-30 18:53:13 +0000

    version bump, package take over

--- COMAK/packages/desktop/gnome3/base/nautilus/actions.py
+++ COMAK/packages/desktop/gnome3/base/nautilus/actions.py
@@ -6,13 +6,16 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
+
+shelltools.export("HOME", get.workDIR())
 
 def setup():
     autotools.autoreconf("-fiv")
-    autotools.configure("--disable-static\
-                         --disable-packagekit\
-                         --disable-update-mimedb\
+    autotools.configure("--disable-static \
+                         --disable-packagekit \
+                         --disable-update-mimedb \
                          --enable-introspection \
                          --disable-more-warnings")
 
