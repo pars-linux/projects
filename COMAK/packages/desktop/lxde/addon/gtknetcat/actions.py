f76ccda480ec73780ad92cf98fdbb8d0c9f1fb86
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-06-05 19:53:30 +0000

    add COPYING files

--- COMAK/packages/desktop/lxde/addon/gtknetcat/actions.py
+++ COMAK/packages/desktop/lxde/addon/gtknetcat/actions.py
@@ -19,5 +19,5 @@ def build():
 def install():
     autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
-    pisitools.dodoc("README", "ChangeLog", "AUTHORS")
+    pisitools.dodoc("README", "COPYING", "AUTHORS")
 
