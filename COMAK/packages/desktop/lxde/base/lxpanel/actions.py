933710ef5d9dbbcc0919a1c96a7815c3bd28843a
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-06-05 19:12:04 +0000

    autoreconf

--- COMAK/packages/desktop/lxde/base/lxpanel/actions.py
+++ COMAK/packages/desktop/lxde/base/lxpanel/actions.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2010 TUBITAK/UEKAE
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import shelltools
 from pisi.actionsapi import get
 
 def setup():
+    autotools.autoreconf("-fiv")
+    shelltools.system("intltoolize --force --copy --automake")
     autotools.configure("--disable-alsa \
-                        --with-x\
+                        --with-x \
                         --with-plugins=all \
                         --enable-man")
 
@@ -20,5 +22,5 @@ def build():
 
 def install():
     autotools.install()
-    pisitools.dodoc("AUTHORS", "ChangeLog", "README")
+    pisitools.dodoc("AUTHORS", "ChangeLog", "COPYING", "README")
 
