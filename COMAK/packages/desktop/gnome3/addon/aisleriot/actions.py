ac1f6606c2f53d9272623262c4fec70080ea0673
necdet.yucel <necdet.yucel@978fe5fe-063b-4661-968b-234df113938b>
2011-06-14 17:37:06 +0000

    fix license and sandbox error

--- COMAK/packages/desktop/gnome3/addon/aisleriot/actions.py
+++ COMAK/packages/desktop/gnome3/addon/aisleriot/actions.py
@@ -6,6 +6,7 @@
 
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
+from pisi.actionsapi import get
 
 def setup():
     autotools.configure("--disable-static")
@@ -14,6 +15,6 @@ def build():
     autotools.make()
 
 def install():
-    autotools.install()
+    autotools.rawInstall("DESTDIR=%s" % get.installDIR())
 
     pisitools.dodoc("COPYING*", "AUTHORS", "ChangeLog")
