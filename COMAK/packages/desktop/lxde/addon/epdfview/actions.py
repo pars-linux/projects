6c2263a4fcff99f00a5c4fe2c1128f685fbd6139
Necdet Yucel <necdetyucel@gmail.com>
2011-06-18 17:45:18 +0000

    add 2009/contrib history

--- COMAK/packages/desktop/lxde/addon/epdfview/actions.py
+++ COMAK/packages/desktop/lxde/addon/epdfview/actions.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 #
-# Copyright 2011 TUBITAK/BILGEM
 # Licensed under the GNU General Public License, version 2.
 # See the file http://www.gnu.org/licenses/old-licenses/gpl-2.0.txt
 
@@ -21,5 +20,5 @@ def build():
 def install():
     autotools.install()
 
-    pisitools.dodoc("AUTHORS", "COPYING")
+    pisitools.dodoc("AUTHORS", "COPYING", "NEWS", "README", "THANKS")
 
